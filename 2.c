88333c4dacf49c94cc453c3322601da0194040df
yellowcrow420 <kirakoz2403@gmail.com>
2025-04-12 12:04:35 +0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+  int blue_balls[5];
+  int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
+  printf("В коробке blue_balls помещается %d шаров. \n", size);
+  return 0;
+}
