e9bf58236c2af54e236905a3cd8d4e490af8cdb9
yellowcrow420 <kirakoz2403@gmail.com>
2025-04-12 12:09:34 +0300

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int colorful_balls[15];
+  for (int i = 0; i < 15; i++)
+    colorful_balls[i] = i;
+  int size = sizeof(colorful_balls) / sizeof(colorful_balls[0]);
+  printf("В коробке помещается шаров. \n", size);
+  return 0;
+}
