4a9505b5aa26ac37984e5767cea022ea50604e25
yellowcrow420 <kirakoz2403@gmail.com>
2025-04-12 12:10:04 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,6 +3,8 @@
 int main() 
 {
   int blue_balls[5];
+  for (int i = 0; i < 5; i++)
+    colorful_balls[i] = i;
   int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
   printf("В коробке blue_balls помещается %d шаров. \n", size);
   return 0;
